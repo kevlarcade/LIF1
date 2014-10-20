9c9fd9f7b00bed9d249ac12f74a1998767759f30
kevlarcade <kevlarcade@gmail.com>
2014-10-20 12:59:45 +0200

    ajout de la table de multiplication

---
+++ 1-6-Table_De_Multiplication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int nb;
+    do{
+        cout << "entrer un nombre entre 1 et 10" << endl;
+        cin >> nb;
+    }
+    while(nb > 10 || nb < 1);
+    cout << "1 * " << nb << " = " << 1*nb << endl << "2 * " << nb << " = " << 2*nb << endl << "3 * " << nb << " = " << 3*nb << endl << "4 * " << nb << " = " << 4*nb << endl << "5 * " << nb << " = " << 5*nb << endl << "6 * " << nb << " = " << 6*nb << endl << "7 * " << nb << " = " << 7*nb << endl << "8 * " << nb << " = " << 8*nb << endl << "9 * " << nb << " = " << 9*nb << endl<< "10 * " << nb << " = " << 10*nb << endl;
+    return 0;
+}
